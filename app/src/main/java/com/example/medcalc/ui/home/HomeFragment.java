b2aa477421797d36312c9edf903b3126a2ab212a
Aleksandr Kuzmenko <dev.skvotcher@yandex.ru>
2022-10-30 18:59:59 +0300

    Допилили шкалы

--- app/src/main/java/com/example/medcalc/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/medcalc/ui/home/HomeFragment.java
@@ -4,19 +4,26 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ExpandableListView;
+import android.widget.SimpleExpandableListAdapter;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.example.medcalc.R;
 import com.example.medcalc.databinding.FragmentHomeBinding;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class HomeFragment extends Fragment {
 
     private FragmentHomeBinding binding;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
+   public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         HomeViewModel homeViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
@@ -24,8 +31,78 @@ public class HomeFragment extends Fragment {
         binding = FragmentHomeBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textHome;
-        homeViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        //setTitle("GCS");
+
+        Map<String, String> map;
+        // коллекция для групп
+        ArrayList<Map<String, String>> groupDataList = new ArrayList<>();
+        // заполняем коллекцию групп из массива с названиями групп
+        String[] mGroupsArray = getResources().getStringArray(R.array.groups);
+        for (String group : mGroupsArray) {
+            // заполняем список атрибутов для каждой группы
+            map = new HashMap<>();
+            map.put("groupName", group); // время года
+            groupDataList.add(map);
+        }
+        // список атрибутов групп для чтения
+        String groupFrom[] = new String[] { "groupName" };
+        // список ID view-элементов, в которые будет помещены атрибуты групп
+        int groupTo[] = new int[] { android.R.id.text1 };
+
+        // создаем общую коллекцию для коллекций элементов
+        ArrayList<ArrayList<Map<String, String>>> сhildDataList = new ArrayList<>();
+
+        // в итоге получится сhildDataList = ArrayList<сhildDataItemList>
+
+        // создаем коллекцию элементов для первой группы
+        ArrayList<Map<String, String>> сhildDataItemList = new ArrayList<>();
+        // заполняем список атрибутов для каждого элемента
+        String[] mEYESArray = getResources().getStringArray(R.array.eyes);
+        for (String month : mEYESArray) {
+            map = new HashMap<>();
+            map.put("monthName", month); // название месяца
+            сhildDataItemList.add(map);
+        }
+        // добавляем в коллекцию коллекций
+        сhildDataList.add(сhildDataItemList);
+       String[] mVERBALArray = getResources().getStringArray(R.array.eyes);
+        // создаем коллекцию элементов для второй группы
+        сhildDataItemList = new ArrayList<>();
+        for (String month : mVERBALArray) {
+            map = new HashMap<>();
+            map.put("monthName", month);
+            сhildDataItemList.add(map);
+        }
+        сhildDataList.add(сhildDataItemList);
+       String[] mMOTORArray = getResources().getStringArray(R.array.eyes);
+        // создаем коллекцию элементов для третьей группы
+        сhildDataItemList = new ArrayList<>();
+        for (String month : mMOTORArray) {
+            map = new HashMap<>();
+            map.put("monthName", month);
+            сhildDataItemList.add(map);
+        }
+        сhildDataList.add(сhildDataItemList);
+
+        // список атрибутов элементов для чтения
+        String childFrom[] = new String[] { "monthName" };
+        // список ID view-элементов, в которые будет помещены атрибуты
+        // элементов
+        int childTo[] = new int[] { android.R.id.text1 };
+
+        SimpleExpandableListAdapter adapter = new SimpleExpandableListAdapter(
+                root.getContext(), groupDataList,
+                android.R.layout.simple_expandable_list_item_1, groupFrom,
+                groupTo, сhildDataList, android.R.layout.simple_list_item_1,
+                childFrom, childTo);
+
+        ExpandableListView expandableListView = (ExpandableListView) root.findViewById(R.id.expListView);
+        expandableListView.setAdapter(adapter);
+        // make the list
+        ArrayList<Boolean> group_check_states = new ArrayList<Boolean> (mGroupsArray.length);
+        for(int i = 0; i < mGroupsArray.length; i++) {
+            group_check_states.add(true);
+        }
         return root;
     }
 
